be05f76ab3019c22ebf81bae69c58f5767bd037d
James Skomsky <james.skomsky@gmail.com>
2019-12-11 14:16:05 -0500

    Chapter 02 updates

---
+++ markjprice/Csharp 8.0 and .NET Core 3.0/Code/Chapter 02/Basics/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // // declare some unused variable usings types
+            // // in additional assemblies
+            // System.Data.DataSet ds;
+            // System.Net.Http.HttpClient client;
+            // // loop trhgou the assemblies that this app references
+            // foreach (var r in Assembly.GetEntryAssembly()
+            //   .GetReferencedAssemblies())
+            // {
+            //     // load the assembly so we can read its details
+            //     var a = Assembly.Load(new AssemblyName(r.FullName));
+
+            //     // declare a variable to count the number of methods
+            //     int methodCount = 0;
+
+            //     // loop through all the types in the assembly
+            //     foreach (var t in a.DefinedTypes)
+            //     {
+            //         // add up the counts of methods
+            //         methodCount += t.GetMethods().Count();
+            //     }
+            //     // output the count of types and their methods
+            //     Console.WriteLine(
+            //         "{0:N0} types with {1:N0} methods in {2} assembly.",
+            //         arg0: a.DefinedTypes.Count(),
+            //         arg1: methodCount,
+            //         arg2: r.Name);
+            // }
+            // // storing text
+            // let the heightInMeters variable become equal to the value 1.88
+            // double heightInMeters = 1.88;
+            // Console.WriteLine($"The variabel {nameof(heightInMeters)} has the value {heightInMeters}.");
+            // //
+            // // assigning literal characters
+            // char letter = 'A';
+            // Console.WriteLine($"the variable \"{nameof(letter)}\" has a value {letter}.");
+            // // assigning verbatim strings
+            // string filepath = @"c:\televisions\sony\bravia.txt";            
+            // Console.WriteLine($"the variable \"{nameof(filepath)}\" has a value {filepath}.");
+        }
+    }
+}
---
+++ markjprice/Csharp 8.0 and .NET Core 3.0/Code/Chapter 02/Numbers/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // // unsigned integer means positive whole number 
+            // // including 0
+            // uint naturalNumber = 23;
+
+            // // integer means negative or psoitive whole number
+            // // including 0
+            // int integerNumber = -23;
+
+            // // float means single-precision floating point
+            // // F suffix makes it a float literal
+            // float realNumber = 2.3F;
+
+            // // double means double-precision floating point
+            // double anotherRealNumber = 2.3;
+
+            // Console.WriteLine($"uint={naturalNumber}, integerNumber={integerNumber}, realNumber={realNumber}, anotherRealNumber={anotherRealNumber}");
+            
+            // // three variables that store the number 2 million
+            // int decNotation = 2_000_000;
+            // int binNotation = 0b_0001_1110_1000_0100_1000_0000;
+            // int hexNotation = 0x_001E_8480;
+
+            // // check the three variables have the same value
+            // // both statements output true
+            // Console.WriteLine($"{decNotation == binNotation}");
+            // Console.WriteLine($"{decNotation == hexNotation}");
+
+            // // writing code to explore number sizes
+            // Console.WriteLine($"int uses {sizeof(int)} bytes can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}");
+            // Console.WriteLine($"double uses {sizeof(double)} bytes can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}");
+            // Console.WriteLine($"decimal uses {sizeof(decimal)} bytes can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}");
+
+            // // comparing double and decimal types
+            // Console.WriteLine("using doubles:");
+
+            // double a = 0.1;
+            // double b = 0.2;
+
+            // if (a + b == 0.3) 
+            // {
+            //     Console.WriteLine($"{a} + {b} equals 0.3");
+            // }
+            // else
+            // {
+            //     Console.WriteLine($"{a} + {b} does not equal 0.3");
+
+            // }
+
+            Console.WriteLine("using doubles:");
+
+            decimal c = 0.1M; // M suffix means a decimal literal value
+            decimal d = 0.2M;
+
+            if (c + d == 0.3M) 
+            {
+                Console.WriteLine($"{c} + {d} equals 0.3");
+            }
+            else
+            {
+                Console.WriteLine($"{c} + {d} does not equal 0.3");
+
+            }
+
+        }
+    }
+}
